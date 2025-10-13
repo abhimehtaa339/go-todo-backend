18b753ad4ea04ebcbab734a6504bf8a81b106861
Abhishek mehta <abhimehtaa339@gmail.com>
2025-10-13 19:12:29 +0530

    Feat: LOGIN

--- models/user.go
+++ models/user.go
@@ -7,11 +7,10 @@ import (
 
 type User struct {
 	gorm.Model
-	Username   string    `json:"username" gorm:"unique;not null"`
+	Email      string    `json:"email" gorm:"unique;not null"`
 	Password   string    `json:"password" gorm:"not null"`
 	FirstName  string    `json:"first_name"`
 	LastName   string    `json:"last_name"`
-	Email      string    `json:"email" gorm:"unique;not null"`
 	Active     bool      `json:"active" gorm:"default:true"`
 	DateJoined time.Time `json:"date_joined" gorm:"autoCreateTime"`
 }
